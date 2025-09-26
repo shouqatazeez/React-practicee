658034b025150d5086cfc50c68196f7b52d35008
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-26 19:38:55 +0530

    some modifications and installlations

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import "./App.css";
+import React from "react";
 import Card from "./components/Card";
-import Counter from "./components/counter";
+import Counter from "./components/Counter";
 
-function App() {
+const App = () => {
   return (
-    <>
-      {/* <Counter /> */}
-
-      <Card
-        username="some tower like structure"
-        description="This is very tall tower looke in a structure of the very tall"
-        btn="click me to know more"
-      />
-      <Card
-        username="Tall Tower"
-        description="look like very talll"
-        btn="view our website"
-      />
-    </>
+    <div>
+      <Card />
+      <Counter />
+    </div>
   );
-}
+};
 
 export default App;
