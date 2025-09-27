4e07d9c6840360d3d62a59ffbe224136c5293a6c
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-09-27 19:26:05 +0530

    work which how we can change the background using the use State

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [color, setColor] = useState("white");
   return (
-    <div>
-      <h1>Hello bahi jaan</h1>
+    <div className="h-screen w-full " style={{ backgroundColor: color }}>
+      <div className="text-center text-black">
+        <h1>Bg-changer</h1>{" "}
+        <div className="border border-white py-5 px-2.5 mt-64 rounded flex flex-wrap  gap-4 fixed text-white  justify-center w-full">
+          <button
+            className="outline rounded-full py-4 px-2 "
+            style={{ background: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="outline rounded-full py-4 px-2 "
+            style={{ background: "green" }}
+            onClick={() => setColor("green")}
+          >
+            green
+          </button>
+          <button
+            className="outline rounded-full py-4 px-2 "
+            style={{ background: "pink" }}
+            onClick={() => setColor("pink")}
+          >
+            pink
+          </button>
+          <button
+            className="outline rounded-full py-4 px-2 "
+            style={{ background: "black" }}
+            onClick={() => setColor("black")}
+          >
+            Black
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
